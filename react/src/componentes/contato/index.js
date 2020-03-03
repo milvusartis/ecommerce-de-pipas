302279c83ec60a487993a6ad7b937d9efb1c2d60
Treinamento <isabelazeitune@gmail.com>
2020-03-03 17:09:15 -0300

    incompleto

--- react/src/componentes/contato/index.js
+++ react/src/componentes/contato/index.js
@@ -9,11 +9,18 @@ import {
     Label,
     Input,
     Button,
+    Modal
  } from 'reactstrap'
 
 class Contato extends Component {
-    
+    state = {
+        show: false
+    }
+        
+
     valEmail = e => {
+        e.preventDefault();
+
         const email = e.target;
 
         var regra1 = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]+?$/i;
@@ -28,12 +35,14 @@ class Contato extends Component {
     }
     
     valTelefone1 = e => {
+        e.preventDefault();
+
         const telefone = e.target;
 
         if(isNaN(telefone.value)) {
             telefone.value = telefone.value.substring(0, (telefone.value.length - 1));
         } else {
-            if(telefone.value.substring(0,1) == 0) {
+            if(telefone.value.substring(0,1) === 0) {
                 telefone.value = telefone.value.substring(0, (telefone.value.length - 1));
             } else {
                 telefone.value = telefone.value.substring(0, 11);
@@ -42,16 +51,39 @@ class Contato extends Component {
     }
 
     valTelefone2 = e => {
+        e.preventDefault();
+
         const telefone = e.target;
 
-        var regraP1 = /^(\d{2})?(\d{4,5})?(\d{4})/g;
-        var regraP2 = "($1) $2-$3"
+        const regraP1 = /^(\d{2})?(\d{4,5})?(\d{4})/g;
+        const regraP2 = "($1) $2-$3"
 
-        if(telefone.value.length == 10 || telefone.value.length == 11) {
+        if(telefone.value.length === 10 || telefone.value.length === 11) {
             telefone.value = telefone.value.replace(regraP1, regraP2);
             telefone.setAttribute("class", "form-control is-valid");
         } else {
             telefone.setAttribute("class", "form-control is-invalid");
+        }
+    }
+
+    handleClose = () => this.setState({show: false});
+    handleShow = () => this.setState({show: true});
+
+    modal = e => {
+        e.preventDefault();
+
+        const botao = e.target;
+        const form = botao.parentElement.parentElement.parentElement;
+        const nomeCompleto = form.children[0].children[1].children[1];
+        const email = form.children[0].children[2].children[1];
+        const telefone = form.children[0].children[3].children[1];
+        const assunto = form.children[0].children[4].children[1];
+        const mensagem = form.children[0].children[5].children[1];
+
+        if(nomeCompleto.value.length > 0 && email.value.length > 0 && email.className == "form-control is-valid" && telefone.value.length > 0 && telefone.className == "form-control is-valid" && assunto.value.length > 0 && mensagem.value.length > 0) {
+            this.handleShow();
+        } else {
+            alert("Preencha corretamente todos os campos para enviar esta mensagem.");
         }
     }
 
@@ -77,10 +109,7 @@ class Contato extends Component {
                                 name="nomeCompleto"
                                 maxLength="50"
                                 placeholder="Digite seu email"
-
-                                //className="form-control"
-                                //required="required"
-                                //id="nomeCompleto"
+                                required="required"
                                 />
                             </FormGroup>
                             <FormGroup>
@@ -92,10 +121,7 @@ class Contato extends Component {
                                 name="email"
                                 placeholder="Digite seu email"
                                 onBlur={this.valEmail}
-
-                                //className="form-control"
-                                //required="required"
-                                //id="email"
+                                required="required"
                                 />
                             </FormGroup>
                             <FormGroup>
@@ -108,10 +134,7 @@ class Contato extends Component {
                                 placeholder="Digite seu telefone"
                                 onKeyUp={this.valTelefone1}
                                 onBlur={this.valTelefone2}
-
-                                //className="form-control"
-                                //required="required"
-                                //id="telefone"
+                                required="required"
                                 />
                             </FormGroup>
                             <FormGroup>
@@ -121,10 +144,7 @@ class Contato extends Component {
                                 <Input
                                 type="select"
                                 name="assunto"
-
-                                //className="form-control"
-                                //required="required"
-                                //id="assunto"
+                                required="required"
                                 >
                                     <option value=""
                                     disabled selected>
@@ -148,30 +168,41 @@ class Contato extends Component {
                                 <Input
                                 type="textarea"
                                 name="mensagem"
-                                rows="4"
+                                rows="3"
                                 maxLength="200"
-                                placeholder="Escreva sua mensagem aqui"
-                                
-                                
-                                //className="form-control"
-                                //required="required"
-                                //id="mensagem"
+                                placeholder="Escreva sua mensagem aqui"                
+                                required="required"
                                 />
                             </FormGroup>
-                            <FormGroup>
+                            <FormGroup className="d-flex justify-content-end">
                                 <Button
                                 type="submit"
                                 name="botao"        
                                 className="btn-success"
-
-                                //required="required"
-                                //id="botao"
+                                onClick={this.modal}
                                 >
                                     Enviar
                                 </Button>
                             </FormGroup>
                         </Col>
                     </Form>
+                    <Modal isOpen={this.state.show}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>
+                                Mensagem enviada com sucesso!
+                            </Modal.Title>
+                        </Modal.Header>
+
+                        <Modal.Body>
+                            <p>Entraremos em contato em até 48h.</p>
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                            <Button oncliCk={this.handleClose}>
+                                Fechar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Container>    
             </>
         )
