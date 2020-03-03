fee996eb02c01ecf7f002c54921c0b40d69436ea
doug3655 <douglas1bl@outlook.com>
2020-03-03 10:33:57 -0300

    estrutura do react

---
+++ prodcompleto/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ProdCompleto from './componentes/prodcompleto';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './css/global.css';
+
+class App extends Component {
+  render() {
+    return < ProdCompleto />
+  }
+}
+
+export default App;
---
+++ prodcompleto/src/componentes/prodcompleto/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ProdCompleto extends Component {
+    
+    render() {
+        return (
+            <p>a</p>
+        )
+    }
+}
+export default ProdCompleto;
