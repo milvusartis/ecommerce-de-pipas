5fa571a98fc92dae2c001cdb4bca1ffd75d1a1dc
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-08 12:22:17 -0300

    Implementando Carousel Controlado

--- src/componentes/main/index.js
+++ src/componentes/main/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import '../../componentes/main';
+import ControlledCarousel from '../../componentes/ControlledCarousel'
+// import './styles.css';
 
 export default class Main extends Component {
  
     render() {
         return (
             <main>
-                <h1>Sou o principal</h1>
+                <ControlledCarousel/>
             </main>
         );
     }
