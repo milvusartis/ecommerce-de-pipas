7e972d2a92fdfb08ff9e55396f6cf4227c47c6f9
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-09 17:57:02 -0300

    Rsolvendo Conflito Index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import ProdCompleto from './componentes/prodcompleto/ProCompleto';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './css/global.css';
-
-class App extends Component {
-  render() {
-    return < ProdCompleto />
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import './css/global.css';
 import Routes from './routes';
@@ -15,7 +6,6 @@ import Routes from './routes';
 class App extends Component {
   render() {
     return <Routes />;
->>>>>>> 1290094139b7c4d06501b3f29c322ad0316e0801
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 ReactDOM.render(
-<<<<<<< HEAD
     <BrowserRouter>
         <App />
     </BrowserRouter>,
-=======
-<BrowserRouter>
-<App />
-</BrowserRouter>,
->>>>>>> 1290094139b7c4d06501b3f29c322ad0316e0801
- document.getElementById('root'));
+document.getElementById('root'));
