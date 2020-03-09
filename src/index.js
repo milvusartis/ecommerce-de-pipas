6a71171486753dddabb2816c1bb5224a39d2d0e2
doug3655 <douglas1bl@outlook.com>
2020-03-09 17:55:27 -0300

    mudei a class app e index

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 ReactDOM.render(
-<<<<<<< HEAD
     <BrowserRouter>
         <App />
     </BrowserRouter>,
-=======
-<BrowserRouter>
-<App />
-</BrowserRouter>,
->>>>>>> 1290094139b7c4d06501b3f29c322ad0316e0801
  document.getElementById('root'));
