bccbf909f27e2445623a96c3944d9c1fb7328fa0
Guilherme <g.h.p.o3615@gmail.com>
2020-03-15 01:57:17 -0300

    front carrinho

--- src/componentes/carrinho/Carrinho.js
+++ src/componentes/carrinho/Carrinho.js
@@ -6,69 +6,61 @@ import "./Carrinho.css";
 
 import {
     Container,
-    Row,
+	Row,
+	Col,
+	ListGroupItem,
+	ListGroup,
+	Button,
+	Input
 } from 'reactstrap';
- 
-
-
-
 class Carrinho extends Component {
+
+
+
+
 
     render() {
         return ( 
         <>
-        <Container>
-            <Row>
-                <div class="container">
-	                <table id="cart" class="table table-hover table-condensed">
-    				<thead>
-						<tr>
-							<th style={{width:50}}> Produtos</th>
-							<th style={{width:10}}>Preco</th>
-							<th style={{width:8}}>Quantidade</th>
-							<th style={{width:22}} class="text-center">Subtotal</th>
-							<th style={{width:10}}>s</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td data-th="Product">
-								<div class="row">
-									<div class="col-sm-2 hidden-xs">
-                                    X
-                                    </div>
-									<div class="col-sm-10">
-										<h4 class="nomargin">Produto 1</h4>
-										<p>Que pipa maravilhosa</p>
-									</div>
-								</div>
-							</td>
-							<td data-th="Price">R$ 22,99</td>
-							<td data-th="Quantity">
-								<input type="number" class="form-control text-center" value="1"/>
-							</td>
-							<td data-th="Subtotal" class="text-center">R$ 22,99</td>
-							<td class="actions" data-th="">
-								<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-							</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr class="visible-xs">
-							<td class="text-center"><strong>Total</strong></td>
-						</tr>
-						<tr>
-							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue comprando conosco</a></td>
-							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Total: R$ PRECO + FRETE</strong></td>
-							<td><a href="#" class="btn btn-success btn-block btn-lg">COMPRAR <i class="fa fa-angle-right"></i></a></td>
-						</tr>
-					</tfoot>
-				</table>
-                </div>
-             </Row>
-        </Container>
+		<Container>
+			<Row>
+				
+			<Col>
+					<Row>
+					<p>Produto</p>
+					<p>Quantidade</p>
+					<p>Entrega</p>
+					<p>Preço</p>
+					</Row>
+					<hr className="my-2" />
+					
+					<Row className="mt-2 mb-2 p-2 qnt">
+                          <Button className="btnMenos" color="success" onClick={this.decrement}>
+                                        -
+                                </Button>
+                                    <Input
+                                        type="number"
+                                        className="col-2 mt-2 quantidade"
+                                        value="0"/>
+                            <Button className="btnMais"color="success" onClick={this.increment}>
+                                        +
+                                </Button>
+                                </Row>
+							</Col>
+						<Col xs="12" md="4">
+							<ListGroup>
+								<h3 className="titulos">Resumo da compra</h3>
+								<ListGroupItem className="listaResumo">Produto:</ListGroupItem>
+								<ListGroupItem className="listaResumo">Valor:</ListGroupItem>
+								<ListGroupItem className="listaResumo">Frete:</ListGroupItem>
+								<ListGroupItem className="listaResumo">Valor total:</ListGroupItem>
+							</ListGroup>
+								<Button  className="btnFinalizar"color="danger">Finalizar Compra</Button>
+							</Col>
+						
+				</Row>
+		</Container>
+       
         </>
         )
     }
