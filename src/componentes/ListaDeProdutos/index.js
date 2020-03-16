e0f64698df2013d87288ca5ae268d2cbb2577844
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-15 21:56:57 -0300

    Adicionando Action para Mudar State Produto

--- src/componentes/ListaDeProdutos/index.js
+++ src/componentes/ListaDeProdutos/index.js
@@ -4,29 +4,40 @@ import { connect } from 'react-redux';
 
 import {
   Col, Row, Container, Card, CardImg, CardTitle, CardText,
-  CardSubtitle, CardBody,
+  CardSubtitle, CardBody, Button,
 } from 'reactstrap';
 
 
 import { Link } from "react-router-dom";
 
-const ListaDeProdutos = ({ produtos }) => (
+function mudaProduto(produto) {
+  return {
+    type: 'MUDAR_PRODUTO_EXIBIDO',
+    produto,
+  };
+
+}
+
+const ListaDeProdutos = ({ produtos, dispatch }) => (
   <>
     <Container>
       <Row>
         {produtos.map(produto => (
 
           <Col className="cardPipas mb-3 mt-3" xs="12" md="4">
-            <Link className="" to="/produto">
+            {/* <Link href="#" className=""  > */}
+            <div>
               <Card key={produto.codigo}>
                 <CardImg top width="100%" src={produto.imagem} alt="Card image cap" />
                 <CardBody>
                   <CardTitle>{produto.nome}</CardTitle>
                   <CardSubtitle className="preco">{produto.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} </CardSubtitle>
                   <CardText></CardText>
+                  <Button onClick={() => dispatch(mudaProduto(produto))}>Visualizar Produto</Button>
                 </CardBody>
               </Card>
-            </Link>
+              </div>
+            {/* </Link> */}
 
           </Col>
 
