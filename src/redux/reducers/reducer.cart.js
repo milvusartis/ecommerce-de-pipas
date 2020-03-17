1edb8a00ad6af929181e04e3866eea81a9c94bae
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-17 14:41:00 -0300

    Refatorando Cart Reducer

--- src/redux/reducers/reducer.cart.js
+++ src/redux/reducers/reducer.cart.js
@@ -3,15 +3,15 @@ import { CartActionsType } from "../actions/actions-type";
 const INITIAL_STATE = {
     novoproduto: {}
 };
-   
+
 
 
 export default function pedido(state = INITIAL_STATE, action) {
-    console.log(action.produto);
-    console.log(action.type);
-    if (action.type === CartActionsType.ADD_TO_CART) {       
-        return { ...state, novoproduto : action.produto}
+    switch (action.type) {
+        case CartActionsType.ADD_TO_CART:
+            return { ...state, novoproduto: action.produto }
+        default:
+            return state;
     }
 
-return state;
 }
