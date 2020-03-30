7d918053dcbff3a606fa5f418d5e7c4c03c3f093
doug36 <douglas1bl@outlook.com>
2020-03-30 09:35:49 -0300

    <Link> adiconados nas paginas para não perder o carrinho

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,33 +25,24 @@ import {
 } from 'reactstrap';
 import { MdAccountCircle, MdSearch } from 'react-icons/md';
 
-
-
 import Badge from 'react-bootstrap/Badge'
 import CarrinhoBadge from "../CarrinhoBadge/CarrinhoBadge";
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
-
-
     const toggle = () => setIsOpen(!isOpen);
-
-  
-      
-
     return (
         <>
             <header>
-                {/* INICIO TOPO */}
-                {/* Para Merge */}
                 <Container fluid={true} id="container">
                     <Row className="topo">
                         <Col className="logo" sm={12} md={3} lg={3}>
-                            <NavbarBrand href="/">
-                                <Logo className="logo" title="Milvus Artis" />
-                            </NavbarBrand>
+                            <Link to="/">
+                                <NavbarBrand>
+                                    <Logo className="logo" title="Milvus Artis" />
+                                </NavbarBrand>
+                            </Link>
                         </Col>
-
                         <Col className="search ml-5 " sm={8} md={3} lg={4}>
                             <InputGroup>
                                 <Input placeholder="Busque por produtos" />
@@ -59,56 +50,44 @@ const Header = () => {
                                     <InputGroupText>
                                         <MdSearch />
                                     </InputGroupText>
-
                                 </InputGroupAddon>
                             </InputGroup>
                         </Col>
-
                         <Col className="target" sm={6} md={3} lg={4}>
                             <Link title="Olá, faça o seu login" to="/login"><Badge pill variant="" className="badge login"><MdAccountCircle /></Badge>{' '}</Link><span className="user">Olá, faça o seu login</span>
                              <CarrinhoBadge/>
                         </Col>                      
-                       
-                       
-
                     </Row>
-
                     <Row>
-
                         <Navbar id="navHome" className="mb-3 navHome" light expand="md">
                             <NavbarToggler onClick={toggle} />
                             <Collapse isOpen={isOpen} navbar>
                                 <Nav className="container mr-auto " navbar>
                                     <NavItem active >
-                                        <NavLink className="btn bg-yellow" href="/">Home</NavLink>
+                                        <Link to="/"><NavLink className="btn bg-yellow" href="/">Home</NavLink></Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink className="btn bg-green" href="/produtos">Produtos</NavLink>
+                                        <Link to="/produtos"><NavLink className="btn bg-green" href="/produtos">Produtos</NavLink></Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink className="btn bg-red" href="/pipas">Pipas</NavLink>
+                                        <Link to="/pipas"><NavLink className="btn bg-red" href="/pipas">Pipas</NavLink></Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink className="btn bg-blue" href="/linhas">Linhas</NavLink>
+                                        <Link to="/linhas"><NavLink className="btn bg-blue" href="/linhas">Linhas</NavLink></Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink className="btn bg-yellow" href="/latas">Latas</NavLink>
+                                        <Link to="/latas"><NavLink className="btn bg-yellow" href="/latas">Latas</NavLink></Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink className="btn bg-red" href="/contato">Contato</NavLink>
+                                        <Link to="/contato"><NavLink className="btn bg-red" href="/contato">Contato</NavLink></Link>
                                     </NavItem>
                                 </Nav>
                             </Collapse>
                         </Navbar>
-
                     </Row>
-
                 </Container>
-
             </header>
         </>
     );
 }
 export default Header;
-
-
