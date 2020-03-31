9e1c1b6703768cdeadf28a72c9209c9b28b47347
Guilherme <g.h.p.o3615@gmail.com>
2020-03-31 12:05:21 -0300

    enviando dados para o banco

--- src/components/Cadastro/Cadastro.js
+++ src/components/Cadastro/Cadastro.js
@@ -7,25 +7,50 @@ import './style.css';
 
 
 export default function Cadastro() {
-    const [name, setName] = useState('');
-    const [lastname, setLastname] = useState('');
+    const [nome, setName] = useState('');
+    const [cpf, setCpf] = useState('');
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
+    const [senha, setSenha] = useState('');
+    const [rg, setRg] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [complemento, setComplemento] = useState('');
+    const [numero, setNumero] = useState('');
+    const [rua, setRua] = useState('');
+    const[uf, setUf] = useState('');
+    const [cep, setCep] = useState('');
+    const [bairro, setBairro] = useState('')
 
 
 async function handleRegister(e){
     e.preventDefault();
 
-    const data = {
-        name,
-        lastname,
-        email,
-        password,
-    };
+    const cliente = {
+        cpf,
+        rg,
+        telefone,
+
+     usuario: {
+            nome,
+            email,
+            senha
+        },
+         endereco :{
+          
+            cidade,
+            complemento,
+            numero,
+            bairro,
+            rua,
+            uf,
+            cep
+        }
+        
+    }
+  
 
     try{
-        const response = await  api.post('cliente', data)
+        const response = await  api.post('clientes', cliente)
         alert("Cadastro realizado com sucesso");
     }catch (err){
         alert('Erro no cadastro, tente novamente.')
@@ -43,13 +68,8 @@ async function handleRegister(e){
                 <form onSubmit={handleRegister}>
                 <input type="text"
                     placeholder="Nome"
-                    value={name}
+                    value={nome}
                     onChange={e => setName(e.target.value)}
-                    />
-                    <input type="text" 
-                    placeholder="Sobrenome"
-                    value={lastname}
-                    onChange={e => setLastname(e.target.value)}
                     />
                     <input type="email" 
                     placeholder="E-mail"
@@ -59,8 +79,62 @@ async function handleRegister(e){
                     <input 
                     placeholder="Senha"
                     type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)}
+                    />
+                    <input type="text" 
+                    placeholder="Cpf"
+                    value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                    />
+                    <input type="text" 
+                    placeholder="Rg"
+                    value={rg}
+                    onChange={e => setRg(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Telefone"
+                    type="tel"
+                    value={telefone}
+                    onChange={e => setTelefone(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Rua"
+                    value={rua}
+                    onChange={e => setRua(e.target.value)}
+                    />
+                    <input type="number"
+                    placeholder="Numero"
+                    value={numero}
+                    onChange={e => setNumero(e.target.value)}
+                    />
+                    <input 
+                    placeholder="Complemento"
+                    value={complemento}
+                    onChange={e => setComplemento(e.target.value)}
+                    />
+                    <div>
+                    <input 
+                    placeholder="Bairro"
+                    value={bairro}
+                    onChange={e => setBairro(e.target.value)}
+                    required
+                    /> 
+                    <input 
+                    placeholder="cidade"
+                    value={cidade}
+                    onChange={e => setCidade(e.target.value)}
+                    />
+                    <input 
+                    placeholder="UF"
+                    value={uf}
+                    onChange={e => setUf(e.target.value)}
+                    />
+                    </div>  
+                    <input 
+                    placeholder="Cep"
+                    value={cep}
+                    onChange={e => setCep(e.target.value)}
                     />
 
                     <button className="button" type="submit">Cadastrar</button>
