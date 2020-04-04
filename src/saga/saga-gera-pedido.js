77a51a2ed4f4325624e2a79d96d787a16e3c3c6e
Douglas Braga Lima <DOBLima@raiadrogasil.com>
2020-04-04 18:53:37 -0300

    WIP - Criando chamada do pedido

---
+++ src/saga/saga-gera-pedido.js
@@ -0,0 +1,30 @@
+import { CartActionsType } from "../redux/actions/actions-type";
+import {all, put, call, takeLatest} from 'redux-saga/effects';
+import api from "../services/api";
+
+function geraJSON(pedido){
+    json = {
+        valorFrete:pedido.valorFrete,
+        diasParaEntrega:pedido.diasEntrega,
+        pedidoItens:pedido.addedItems,
+        
+    }
+    return json;
+}
+
+function* geraPedidoRequested(pedido){
+    const response = yield call (api.post,geraJSON(pedido));
+}
+
+function* whatchRequestGeraPedido(){
+    yield takeLatest(  CartActionsType.GERA_PEDIDO, geraPedidoRequested)
+}
+
+function* realizaPedido(){
+    yield all([
+         whatchRequestGeraPedido(),
+     ])
+
+ }
+
+export{realizaPedido} 
