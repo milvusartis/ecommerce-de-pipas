898d2446f401a6a01943a8e31192e1d32bedf273
Guilherme Henrique Pereira de Oliveira <GPOliveira@raiadrogasil.com>
2020-04-08 14:51:04 -0300

    merge

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -19,7 +19,7 @@ export default function Login() {
         const credenciais = btoa(JSON.stringify({"username":email, "password":senha}))
         sessionStorage.setItem("credenciais", credenciais)
 
-       
+
 
         api.get('/auth/token', {
         
