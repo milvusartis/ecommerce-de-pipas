53bef3fc10b12885e1bb65b91d663d86214c8ede
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-04-08 17:20:26 -0300

    Buscando Pedidos do Usuario

--- src/components/ItemHistoricoDePedido/ItemHistoricoDePedido.js
+++ src/components/ItemHistoricoDePedido/ItemHistoricoDePedido.js
@@ -7,7 +7,6 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import Typography from '@material-ui/core/Typography';
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 
@@ -17,9 +16,6 @@ import './ItemHistoricoDePedido.scss'
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-    },
     heading: {
         fontSize: theme.typography.pxToRem(15),
     },
@@ -53,96 +49,93 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-export default function ItemHistoricoDePedido() {
+export default function ItemHistoricoDePedido({ key, pedido }) {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
+    const pedidoItens = pedido.pedidoItens;
+    console.log(pedido)
+
 
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
 
+
     return (
         <>
-                    <div className={classes.root}>
-
-                    
-                        <ExpansionPanel TransitionProps={{ unmountOnExit: true }} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                            <ExpansionPanelSummary
-                                expandIcon={<MdExpandMore />}
-                                aria-controls="panel1c-content"
-                                id="panel1c-header">
-                                <div className={classes.column}>
-                                    <Typography className={classes.heading}>Pedido: #1234</Typography>
-                                </div>
-                                <div className={classes.column}>
-                                    <Typography className={classes.secondaryHeading}>ooba, pedido entregue</Typography>
-                                </div>
-                            </ExpansionPanelSummary>
-
-                            <ExpansionPanelDetails className={classes.details}>
-                                <ol className="progress-track" data-steps="4">
-                                    <li className="done">
-                                        <span className="name">Pedido realizado</span>
-                                        <span className="step"><span>1</span></span>
-                                    </li>
-                                    <li className="done">
-                                        <span className="name">Pagamento confirmado</span>
-                                        <span className="step"><span>2</span></span>
-                                    </li>
-                                    <li className="active">
-                                        <span className="name">Pedido enviado</span>
-                                        <span className="step"><span>3</span></span>
-                                    </li>
-                                    <li>
-                                        <span className="name">Pedido entregue</span>
-                                        <span className="step"><span>4</span></span>
-                                    </li>
-                                </ol>
-                            </ExpansionPanelDetails>
-
-                            <ExpansionPanelDetails className={classes.details}>
-                                {/* <div className={classes.column} /> */}
-                                <div className={classes.column}>
-                                    {/* <Chip label="Barbados" onDelete={() => {}} /> */}
-                                    <img width="80" src="https://i.ibb.co/SvxNRNy/pipa-box.png" alt="" />
-                                </div>
-                                <div className={clsx(classes.column, classes.helper)}>
-                                    <Typography variant="caption">
-                                        <p>Pipa Grande Colorida</p>
-                                        <strong>2 und - R$ 50,00</strong>
-                                        <p>Subtotal: R$ 100</p>
-                                    </Typography>
-                                </div>
-                            </ExpansionPanelDetails>
 
 
-                            <ExpansionPanelDetails className={classes.details}>
-                                <div className={classes.column}>
-                                    {/* <Chip label="Barbados" onDelete={() => {}} /> */}
-                                    <img width="80" src="https://i.ibb.co/SvxNRNy/pipa-box.png" alt="" />
-                                </div>
-                                <div className={clsx(classes.column, classes.helper)}>
-                                    <Typography variant="caption">
-                                        <p>Pipa Grande Colorida</p>
-                                        <strong>2 und - R$ 50,00</strong>
-                                        <p>Subtotal: R$ 100</p>
-                                    </Typography>
-                                </div>
-                            </ExpansionPanelDetails>
 
-                            <Divider />
-
-                            <ExpansionPanelActions>
-                                {/* <Button size="small">Cancel</Button> */}
-                                <strong >Total - R$ 200,00</strong>
-                                <Button size="small" color="primary">
-                                    Refazer pedido
-                                 </Button>
-                            </ExpansionPanelActions>
-                        </ExpansionPanel>
-
-                       
+            <ExpansionPanel TransitionProps={{ unmountOnExit: true }} expanded={expanded === `panel${key}`} onChange={handleChange(`panel${key}`)}>
+                <ExpansionPanelSummary
+                    expandIcon={<MdExpandMore />}
+                    aria-controls={`panel${key}c-content`}
+                    id={`panel${key}c-header`}>
+                    <div className={classes.column}>
+                        <Typography className={classes.heading}>Pedido: #{pedido.idPedido}</Typography>
                     </div>
+                    <div className={classes.column}>
+                        <Typography className={classes.secondaryHeading}>ooba, pedido entregue</Typography>
+                    </div>
+                </ExpansionPanelSummary>
+
+                <ExpansionPanelDetails className={classes.details}>
+                    <ol className="progress-track" data-steps="4">
+                        <li className="done">
+                            <span className="name">Pedido realizado</span>
+                            <span className="step"><span>1</span></span>
+                        </li>
+                        <li className="done">
+                            <span className="name">Pagamento confirmado</span>
+                            <span className="step"><span>2</span></span>
+                        </li>
+                        <li className="active">
+                            <span className="name">Pedido enviado</span>
+                            <span className="step"><span>3</span></span>
+                        </li>
+                        <li>
+                            <span className="name">Pedido entregue</span>
+                            <span className="step"><span>4</span></span>
+                        </li>
+                    </ol>
+                </ExpansionPanelDetails>
+
+
+                {pedidoItens.map(pedidoItem => (
+
+
+
+                    <ExpansionPanelDetails className={classes.details}>
+                        {/* <div className={classes.column} /> */}
+                        <div className={classes.column}>
+                            {/* <Chip label="Barbados" onDelete={() => {}} /> */}
+                            <img width="80" src={pedidoItem.produto.imagem} alt="" />
+                        </div>
+                        <div className={clsx(classes.column, classes.helper)}>
+                            <Typography variant="caption">
+                                <p>{pedidoItem.produto.nome}</p>
+                                <strong>{pedidoItem.quantidade} und - {pedidoItem.precoVendido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+                                <p>Subtotal: {((pedidoItem.quantidade)*(pedidoItem.precoVendido)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+                            </Typography>
+                        </div>
+                    </ExpansionPanelDetails>
+
+                ))}
+
+
+                <Divider />
+
+                <ExpansionPanelActions>
+                    {/* <Button size="small">Cancel</Button> */}
+                <strong >Total - {pedido.valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+                    <Button size="small" color="primary">
+                        Refazer pedido
+                                 </Button>
+                </ExpansionPanelActions>
+            </ExpansionPanel>
+
+
+
 
 
 
--- src/pages/historicoPedidos/historicoPedidos.js
+++ src/pages/historicoPedidos/historicoPedidos.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemHistoricoDePedido from '../../components/ItemHistoricoDePedido/ItemHistoricoDePedido';
+import api from '../../services/api'
+import { makeStyles } from '@material-ui/core/styles';
 
 import './historicoPedidos.scss'
 
+const { idUsuario } = JSON.parse(sessionStorage.getItem('usuario'));
 
-export default function HistoricoPedidosl() {   
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+}));
+
+
+
+export default function HistoricoPedidosl() {
+
+    const classes = useStyles();
+    const [pedidos, setPedidos] = useState([]);
+
+
+    useEffect(() => {
+        api.get(`historico-pedidos/4`, {
+
+        }).then(response => {
+            // setPedidos(response.data)
+            setPedidos(response.data);
+
+        })
+    }, [idUsuario]);
+
+
+
+    // api.get('/auth/token', {
+
+    // }).then((response => {
+    //     sessionStorage.setItem("usuario", JSON.stringify(response.data))
+    //     history.push("/")
+    //     window.location.reload();
+
+
+    // })).catch((error) => {
+    //     if (401 === error.response.status){
+    //         return alert("Usuário ou senha não conferem");
+    // }
+    //     alert("Erro não esperado");
+    // });
+
+
 
     return (
         <>
             <div className="pedido-container">
                 <div className="content">
                     <h1>Pedidos realizados</h1>
-                    <ItemHistoricoDePedido/>
+                    <div className={classes.root}>
+                        {pedidos.map(pedido => (
+                            <ItemHistoricoDePedido key={pedido.idPedido} pedido={pedido} />
+                        ))}
+
+                    </div>
                 </div>
             </div>
         </>
