888a7038ebb1f483728c06c86e6eedcea3d02620
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-04-15 16:55:22 -0300

    Gerando nova senha

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,23 @@
-import axios, {AxiosResponse} from "axios";
+import axios from "axios";
+import { history } from "./../index";
 import handleLogout from "./logout";
 import { toast } from "react-toastify";
 
-const api = axios.create({baseURL:'http://localhost:8080/api/ecommerce'});
+const api = axios.create({ baseURL: 'http://localhost:8080/api/ecommerce' });
 
 
-api.interceptors.request.use(function(config) {
+api.interceptors.request.use(function (config) {
     // const decodificar = sessionStorage.getItem("credenciais");
     // if(decodificar!==null){
     //     config.auth = JSON.parse(atob(decodificar))
     // }
 
     const token = sessionStorage.getItem("token");
-    if(token!==null){
-        config.headers['Authorization'] =  JSON.parse(token);
+    if (token !== null) {
+        config.headers['Authorization'] = JSON.parse(token);
     }
-   
-return config;
+
+    return config;
 });
 
 
@@ -24,59 +25,42 @@ return config;
 api.interceptors.response.use((response) => {
     return response;
 }, function (error) {
-    // Do something with response error
 
-   
-    if(error.mmessage === 'Network Error' && !error.response){
-        toast.error('falha na conexão com o servidor!') 
-       
+    if (error.mmessage === 'Network Error' && !error.response) {
+        toast.error('falha na conexão com o servidor!');
     }
-   
-     
-   
 
-    const {status, data, config} = error.response;
+    const { status, data, config } = error.response;
 
-    if (status === 401) {           
+    if (status === 401) {      
         handleLogout();
-        toast.error('Credenciais inválidas') 
-       
-        // window.location.reload();
-        // auth.logout();
-        // router.replace('/auth/login');
+        history.push("/login")        
+        sessionStorage.setItem("msg", btoa("pssInvalid"));       
+        toast.error('Falha no Login, tente novamente');
+     
+    
     }
 
-    if (status === 403) {       
-        toast.error('Acesso não autorizado') 
-       
-       
-        // window.location.reload();
-        // auth.logout();
-        // router.replace('/auth/login');
+    if (status === 403) {
+        handleLogout();
+        toast.error('Acesso não autorizado');
     }
 
-    if(status ===404){
-       console.log("404")
+    if (status === 404) {
+        toast.error('Houston we have a problem');
     }
 
-    if(status===400 && config.method === 'get' && data.error.hasOwnProperty('id')){
-        console.log("400")
+    if (status === 400 && config.method === 'get' && data.error.hasOwnProperty('id')) {
+        history.push("/");
     }
 
-    if(status===500){
-        toast.warn('Erro no servidor') 
+    if (status === 500) {
+        toast.warn('Erro no servidor');
     }
 
     return Promise.reject(error.response);
 });
 
-
-
-
-
 export default api;
 
 
- // toast.success("Success Notification !")
-        // toast.warn("Warning Notification !")
-        // toast.info("Info Notification !")
