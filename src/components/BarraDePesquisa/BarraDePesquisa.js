efb5f14ea173abeda55e92e32f66833a50a60053
diogenesistemas <ads.diogenes@gmail.com>
2020-04-16 12:16:04 -0300

    Revert "barra de busca concluida"

--- src/components/BarraDePesquisa/BarraDePesquisa.js
+++ src/components/BarraDePesquisa/BarraDePesquisa.js
@@ -3,92 +3,23 @@ import {
     InputGroup,
     Input,
     InputGroupAddon,
-    InputGroupText,
-    ListGroup,
-    ListGroupItem
+    InputGroupText
 } from 'reactstrap';
-import { MdClose } from 'react-icons/md';
-import './BarraDePesquisa.scss'
-import * as ProductAction from '../../redux/actions/action.product'
-import { listaDeProdutoActions } from '../../redux/actions/action.lista-de-produtos'
-import { connect } from 'react-redux';
+import { MdSearch } from 'react-icons/md';
 
-class BarraDePesquisa extends Component {
-    state = {
-        resultado: '',
-        mostra: '',
-        query: ''
-    }
-    componentDidMount() {
-        const { getAll } = this.props;
-        getAll();
-    }
-    handleInput = (event, produtos) => {
-        this.setState({ mostra: '' })
-        let query = event.target.value
-        this.setState({ query: query })
-        const { produtoAtivo, changeDisplayedProduct } = this.props
-        if (event.target.value === "") {
-            this.setState({ resultado: '' })
-        } else {
-            let valor = event.target.value
-            let filtro = produtos.filter((el) => {
-                return el.nome.toLowerCase().includes(valor.toLowerCase())
-            })
-            let produtosNome = filtro.map(res => (
-                <ListGroupItem
-                    key={res.idProduto}
-                    onClick={() => {
-                        changeDisplayedProduct(res, produtoAtivo)
-                        this.setState({ mostra: "esconde" })
-                        this.setState({ query: res.nome })
-                    }}
-                >
-                    {res.nome}
-                </ListGroupItem>)
-            )
-            let exibir = (
-                <ListGroup className="bloco">
-                    {produtosNome}
-                </ListGroup>
-            )
-            this.setState({ resultado: exibir })
-            console.log(exibir)
-        }
-    }
+export default class BarraDePesquisa extends Component {
     render() {
-        const { produtos } = this.props
-        const { resultado, mostra, query } = this.state
-        return (
+        return(
             <>
                 <InputGroup>
-                    <Input placeholder="Busque por produtos" onChange={(event) => { this.handleInput(event, produtos) }} value={query} />
-                    {query !== '' &&
-                        <InputGroupAddon addonType="append">
-                            <InputGroupText>
-                                <MdClose onClick={() => { this.setState({ query: '' }) }} />
-                            </InputGroupText>
-                        </InputGroupAddon>
-                    }
+                <Input placeholder="Busque por produtos" />
+                <InputGroupAddon addonType="append">
+                    <InputGroupText>
+                        <MdSearch />
+                    </InputGroupText>
+                </InputGroupAddon>
                 </InputGroup>
-                <div className={mostra === '' ? "resultado" : mostra}>
-                    {resultado}
-                </div>
             </>
         )
     }
 }
-
-const mapStateToProps = state => ({
-    produtos: state.listaDeProdutosReducer.produtos,
-    produtoAtivo: state.produtoReducer.produtoAtivo
-})
-
-const mapDispatchToProps = dispatch => ({
-    getAll: () => dispatch(listaDeProdutoActions.requestGetAll()),
-    changeDisplayedProduct: (produto, produtoAtivo) => dispatch(ProductAction.changeDisplayedProduct(produto, produtoAtivo)),
-})
-
-export default connect(
-    mapStateToProps, mapDispatchToProps
-)(BarraDePesquisa);
