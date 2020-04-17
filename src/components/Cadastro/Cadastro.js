355683e50d3904b9752770ba997c99535913fa98
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-04-17 13:20:37 -0300

    Adicionando validações no cadastro

--- src/components/Cadastro/Cadastro.js
+++ src/components/Cadastro/Cadastro.js
@@ -32,28 +32,102 @@ export default function Cadastro() {
     const history = useHistory();
 
 
-    
+
 
     const handdleEndereco = (cep) => {
         apiViacep.get(cep + '/json', {
 
-        }).then((response => {           
-            if(response.data.erro){
+        }).then((response => {
+            if (response.data.erro) {
                 toast.error("Digite um cep válido")
-            }   
-            const endereco =  response.data;  
-         
+            }
+            const endereco = response.data;
+
             setRua(endereco.logradouro);
             setComplemento(endereco.complemento);
             setBairro(endereco.bairro);
             setCidade(endereco.localidade);
-            setUf(endereco.uf);           
+            setUf(endereco.uf);
         }))
         // .catch((error) => {
-          
-            
-            
+
+
+
         // });
+    }
+
+
+
+    const validaCPF = (strCPF) => {
+        let soma;
+        let resto;
+        let cpf = ""
+
+        for (let i = 0; i < strCPF.toString().length; i++) {
+            let char = strCPF.substring(i, i + 1);
+            if (char !== "." && char !== "-")
+                cpf += char;
+        }
+        soma = 0;
+        if (cpf === "00000000000" || cpf === "11111111111" || cpf === "22222222222" || cpf === "33333333333" || cpf === "44444444444" || cpf === "55555555555" || cpf === "66666666666" || cpf === "77777777777" || cpf === "88888888888" || cpf === "99999999999")
+            return false;
+
+        for (let i = 1; i <= 9; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        resto = (soma * 10) % 11;
+
+
+
+        if ((resto === 10) || (resto === 11))
+            resto = 0;
+        if (resto !== parseInt(cpf.substring(9, 10)))
+            return false;
+
+
+
+        soma = 0;
+        for (let i = 1; i <= 10; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        resto = (soma * 10) % 11;
+
+        if ((resto === 10) || (resto === 11))
+            resto = 0;
+        if (resto !== parseInt(cpf.substring(10, 11)))
+            return false;
+
+        return true;
+
+
+    }
+
+
+    const verificaCampos = (campo, qtd) => {
+        const validador = campo.replace(/[^0-9]+/g, '');
+        return (validador !== "" && validador.length === qtd)
+    }
+
+    const verificaCPF = (e) => {
+        const campo = e.target.value
+        const cpf = campo.replace(/[^0-9]+/g, '');
+
+
+        if (!verificaCampos(campo, 11)) {
+            e.target.focus()
+            toast.warn("Por favor, digite um cpf com 11 dígitos")
+        } else {
+            if (!validaCPF(cpf)) {
+                e.target.focus()
+                toast.warn("Opa! Esse CPF não é válido")
+            }
+        }
+    }
+
+    const verificaTelefone = (e) => {
+        const campo = e.target.value
+        if (!verificaCampos(campo, 10)) {
+            e.target.focus()
+            toast.warn("Digita um telefone válido")
+        }
     }
 
 
@@ -63,7 +137,6 @@ export default function Cadastro() {
         const cepSemMascara = cep.replace(/[^0-9]+/g, '');
         //Valida se o cep tem 8 dígitos
         (cepSemMascara !== "" && cepSemMascara.length === 8) ? (handdleEndereco(cepSemMascara)) : toast.warn("Cep não preenchido completamente");
-
     }
 
     async function handleRegister(e) {
@@ -174,6 +247,7 @@ export default function Cadastro() {
                             required
                             value={cpf}
                             onChange={e => setCpf(e.target.value)}
+                            onBlur={e => { verificaCPF(e) }}
                         />
                         <InputMask
                             type="text"
@@ -188,7 +262,7 @@ export default function Cadastro() {
                             placeholder="Telefone"
                             type="tel"
                             required
-
+                            onBlur={e => { verificaTelefone(e) }}
                             value={telefone}
                             onChange={e => setTelefone(e.target.value)}
                         />
